bdcbfcf3056c5953dce95321f4880f810b97142c
miuxo <shammas7809@gmail.com>
2025-12-23 20:53:05 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -6,3 +6,7 @@ self.addEventListener('fetch', (e) => {
  // This allows the app to load from cache later
  e.respondWith(fetch(e.request));
 });
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('sw.js');
+}
